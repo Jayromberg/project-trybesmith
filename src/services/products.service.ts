d5ad511b7057df47c299fe4196ca3c8cc31a2545
Jayromberg <jayro.dev@gmail.com>
2022-10-06 17:16:23 -0300

    feat: Add products.service e sua função de registro de produtos

---
+++ src/services/products.service.ts
@@ -0,0 +1,17 @@
+import Product from '../interfaces/products.interfaces';
+import connection from '../models/connection';
+import ProductModel from '../models/products.model';
+
+export default class ProductService {
+  private model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  }
+
+  public async registerProduct(product: Product): Promise<Product> {
+    const registeredProduct = await this.model.create(product);
+
+    return registeredProduct;
+  } 
+}
